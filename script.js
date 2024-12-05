faf592ee3f9075c6344af6f942cc285e29eb12ab
sengvang.blo <skvgithub@gmail.com>
2024-12-05 08:46:25 +0700

    first time

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('countButton').addEventListener('click', function() {
+    // Get the input text value
+    const inputText = document.getElementById('inputText').value;
+  
+    // Count the number of characters (or numbers)
+    const count = inputText.length;
+  
+    // Display the result
+    document.getElementById('count').textContent = count;
+  });
+  
