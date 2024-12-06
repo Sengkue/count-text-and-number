d4e7783386fef667950581ae8a92928fed6a85f7
sengvang.blo <skvgithub@gmail.com>
2024-12-06 08:29:53 +0700

    commit Fri 12/06/2024  8:29:53.18

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('countButton').addEventListener('click', function() {
   // Get the input text value
   const inputText = document.getElementById('inputText').value;
 
-  // Count the number of characters
+  // Count the number of characters (or numbers)
   const count = inputText.length;
 
   // Display the result
